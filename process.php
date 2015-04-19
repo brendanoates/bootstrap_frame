b83ca66b579b93318b05150c121b55a34bc9eae7
Brendan HomePC <brenoates@gmail.com>
2015-04-19 20:21:31 +0100

    article pages and contact form.

---
+++ process.php
@@ -0,0 +1,21 @@
+<?php
+//add the recipient's address here
+$myemail = 'brenoates@gmail.com;
+
+//grab named inputs from html then post to #thanks
+if (isset($_POST['contact_name'])) {
+$name = strip_tags($_POST['contact-name']);
+$email = strip_tags($_POST['contact-email']);
+$message = strip_tags($_POST['contact-message']);
+
+//generate email and send!
+$to = $myemail;
+$email_subject = "Contact form submission: $name";
+$email_body = "You have received a new message. ".
+" Here are the details:\n Name: $name \n ".
+"Email: $email\n Message \n $message";
+$headers = "From: $myemail\n";
+$headers .= "Reply-To: $email";
+mail($to,$email_subject,$email_body,$headers);
+}
+?>
