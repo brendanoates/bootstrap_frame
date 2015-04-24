a4c737a3e12e4a9cdae76010a6a4603c4e23c9c0
Brendan HomePC <brenoates@gmail.com>
2015-04-24 23:17:42 +0100

    published site

--- process.php
+++ process.php
@@ -1,12 +1,12 @@
 <?php
-//add the recipient's address here
-$myemail = 'brenoates@gmail.com;
-
+$url = 'http://carleyma.co.uk';
+//$myemail = 'carley_oates@yahoo.com';
+$myemail = 'brenoates@gmail.com';
 //grab named inputs from html then post to #thanks
-if (isset($_POST['contact_name'])) {
-$name = strip_tags($_POST['contact-name']);
-$email = strip_tags($_POST['contact-email']);
-$message = strip_tags($_POST['contact-message']);
+if (isset($_POST['contact_form_name'])) {
+$name = strip_tags($_POST['contact_form_name']);
+$email = strip_tags($_POST['contact_form_email']);
+$message = strip_tags($_POST['contact_form_message']);
 
 //generate email and send!
 $to = $myemail;
@@ -18,4 +18,5 @@ $headers = "From: $myemail\n";
 $headers .= "Reply-To: $email";
 mail($to,$email_subject,$email_body,$headers);
 }
+header("Location: $url");
 ?>
